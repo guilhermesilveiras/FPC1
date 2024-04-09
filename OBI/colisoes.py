454b1be4ceb9b6270e2bf406768c2b8dc528a162
Guilherme Salgueiro Silveira <127560908+guilhermesilveiras@users.noreply.github.com>
2024-04-08 22:50:39 -0300

    Colisão ‼️

---
+++ OBI/colisoes.py
@@ -0,0 +1,9 @@
+coord1 = [int(i) for i in input().split()]
+coord2 = [int(i) for i in input().split()]
+
+
+
+if coord1[0] < coord2[0] and coord1[2] < coord2[2] and coord1[0] < coord2[2] and coord1[2] < coord2[0] or coord1[0] > coord2[0] and coord1[2] > coord2[2] and coord1[0] > coord2[2] and coord1[2] > coord2[0]:
+    print('0')
+else:
+    print('1')
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # FPC1
  Fundamentos de Problemas Computacionais 1
+
+Questões e estudos de Algoritmo e Estrutura de Dados.
