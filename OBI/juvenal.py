9e978f9ccefdfe2df481d5799cc854fecd613e7f
Guilherme Salgueiro Silveira <127560908+guilhermesilveiras@users.noreply.github.com>
2024-04-10 00:58:05 -0300

    Juvenal

--- OBI/juvenal.py
+++ OBI/juvenal.py
@@ -1,7 +1,28 @@
+count = 0
+def G(contador):
+    global count
+    count += contador
+    return count
+
 
 def f(n):
     while n != 1:
         if n%2 == 0:
+            G(1)
             return f(n/2)
-        elif n%2 !== 0:
+        elif n%2 != 0:
+            G(1)
             return f(3*n+1)
+    if n == 1:
+        return G(0)
+        
+T = int(input("Digite o número de casos: "))
+casos = [int(x) for x in input().split()]
+
+for i in range(T):
+    if f(casos[2*i]) > f(casos[2*i+1]):
+        print(f'Caso {i+1}: {f(casos[2*i])}')
+    else:
+        print(f'Caso {i+1}: {f(casos[2*i+1])}')
+
+
