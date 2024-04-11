b795bb72fb3588d1033c4b8ea756bf45e5c99be9
Guilherme Salgueiro Silveira <127560908+guilhermesilveiras@users.noreply.github.com>
2024-04-10 21:43:18 -0300

    update colisoes.py

--- OBI/colisoes.py
+++ OBI/colisoes.py
@@ -2,7 +2,7 @@ coord1 = [int(i) for i in input().split()]
 coord2 = [int(i) for i in input().split()]
 
 
-if coord1[0] < coord2[0] and coord1[2] < coord2[2] and coord1[0] < coord2[2] and coord1[2] < coord2[0] or coord1[0] > coord2[0] and coord1[2] > coord2[2] and coord1[0] > coord2[2] and coord1[2] > coord2[0]:
+if coord1[2] < coord2[0] or coord1[0] > coord2[2] or coord1[3] < coord2[1] or coord1[1] > coord2[3]:
     print('0')
 else:
     print('1')
