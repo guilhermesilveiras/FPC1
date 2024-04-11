ef17b690413e60fcb70f70e07611ab71efd0b9c6
Guilherme Salgueiro <127560908+guilhermesilveiras@users.noreply.github.com>
2024-04-11 12:38:14 +0000

    Fibonacci muito esperto!

--- OBI/fibonacci.py
+++ OBI/fibonacci.py
@@ -2,10 +2,11 @@ def fib(n):
     if n < 1:
         return 0
     a, b, r = 0, 1, 1
-    for i in range(2, n+1):
+    for i in range(2, n):
         a = b 
         b = r
         r = a + b
     return r
 
-fib(50)
+x = int(input())
+print(fib(x))
