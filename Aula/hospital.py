9ed45d4dde25546054ad2c3ca25d87b2fad97b0c
Guilherme Salgueiro <127560908+guilhermesilveiras@users.noreply.github.com>
2024-08-01 15:01:43 +0000

    1a parte da questão do hospital

---
+++ Aula/hospital.py
@@ -0,0 +1,23 @@
+#questão hospital resolvido por rodrigo (quicksort)
+
+def comparacao(paciente_1: Tuple[str, int, str]
+
+
+
+def partition(A, p, r):
+    x = A[r]
+    i = p - 1
+    for j in range(p, r):
+        if A[j] <= x:
+            i += 1
+            A[i], A[j] = A[j], A[i]
+    A[i+1], A[r] = A[r], A[i+1]
+    return i + 1
+
+#quicksort
+
+def quicksort(A, p, r):
+    if p < r:
+        q = partition(A, p, r)
+        quicksort(A, p, q - 1)
+        quicksort(A, q + 1, r)
